ce14e38ec0f90ac32eac7ee6ffc77d8ba26bfd8f
Saeid <Saeed.modarresi.dev@gmail.com>
2024-12-03 08:54:47 -0330

    Create an abstract Model class to be extended by other models, providing built-in ORM features.

---
+++ app/Src/Abstracts/Model.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Wizban\Theme\Src\Abstracts;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+abstract class Model extends BaseModel
+{
+
+    public function __construct()
+    {
+        global $wpdb;
+
+        $capsule = new Capsule();
+
+        $capsule->addConnection([
+            "driver" => "mysql",
+            "host"   => DB_HOST,
+            "database" => DB_NAME,
+            "username" => DB_USER,
+            "password" => DB_PASSWORD,
+            'charset'  => DB_CHARSET,
+            'prefix'   => $wpdb->prefix
+        ]);
+
+        $capsule->setAsGlobal();
+
+        $capsule->bootEloquent();
+    }
+}
